3a7aee834cce8d2ca1a69393a85cb28d861afbc6
Rishi2333 <113488724+Rishi2333@users.noreply.github.com>
2025-02-10 08:57:16 +0530

    Time: 0 ms (100%), Space: 9 MB (41.76%) - LeetHub

---
+++ leetcode/3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+
+        for(char &ch : s){
+            if (ch >= 'a' && ch <= 'z'){
+                st.push(ch);
+            }else if(!st.empty()){
+                st.pop();
+            }
+        }
+
+        string result = "";
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+        reverse(begin(result), end(result));
+
+        return result;
+    }
+};
